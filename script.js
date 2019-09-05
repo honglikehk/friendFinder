d590f5db60657c622c0d496939332b65329ef4c2
honglikehk <christine.hong.to@gmail.com>
2019-09-04 21:12:56 -0400

    removed html

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
 // app.get("/", function(req, res) {
-//   res.json(path.join(__dirname, "public/index.html"));
+//   res.sendFile(path.join(__dirname, "index.html"));
 // });
+
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
 
 // Starts the server to begin listening
 // =============================================================
